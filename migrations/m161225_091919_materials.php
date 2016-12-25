ab5df29dce302893a94cca231fa2e70000613a6e
alpo008 <pozhydaev_alexey@mail.ru>
2016-12-25 13:13:33 +0300

    table materials added

---
+++ migrations/m161225_091919_materials.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\db\Migration;
+
+class m161225_091919_materials extends Migration
+{
+/*    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m161225_091919_materials cannot be reverted.\n";
+
+        return false;
+    }*/
+
+
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+        $this->createTable('materials', [
+            'id' => $this->primaryKey(4)->notNull(),
+            'ref' => $this->integer(8)->defaultValue(NULL)->unique(),
+            'name' => $this->string(128)->defaultValue(NULL),
+            'qty' => $this->decimal(7,3)->defaultValue(NULL),
+            'minqty' => $this->decimal(7,3)->defaultValue(1.000),
+            'unit' => $this->string(3)->defaultValue(NULL),
+            'type' => $this->string(8)->defaultValue(NULL),
+            'gruppa' => $this->integer(3)->unsigned()->defaultValue(0),
+        ]);
+        
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('materials');
+    }
+
+}
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -36,11 +36,11 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => Yii::t('app','Home'), 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => Yii::t ('app','Home'), 'url' => ['/site/index']],
+            ['label' => Yii::t ('app', 'About'), 'url' => ['/site/about']],
+            ['label' => Yii::t ('app','Contact'), 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
+                ['label' => Yii::t ('app','Login'), 'url' => ['/site/login']]
             ) : (
                 '<li>'
                 . Html::beginForm(['/site/logout'], 'post')
