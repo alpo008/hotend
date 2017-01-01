3f56dc076a73c0dc6457cdbaa5c74308690eb63f
alpo008 <pozhydaev_alexey@mail.ru>
2017-01-01 17:49:24 +0300

    generated model for the junction table LOcations

---
+++ models/Locations.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\models;
+
+use yii;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "locations".
+ *
+ * @property integer $materials_id
+ * @property integer $stocks_id
+ * @property string $qty
+ */
+class Locations extends ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'locations';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['materials_id', 'stocks_id'], 'integer'],
+            [['qty'], 'number'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'materials_id' => Yii::t('app', 'Materials ID'),
+            'stocks_id' => Yii::t('app', 'Stocks ID'),
+            'qty' => Yii::t('app', 'Qty'),
+        ];
+    }
+
+    public function getMaterials()
+    {
+        return $this->hasOne(Materials::className(), ['id' => 'materials_id']);
+    }
+    
+    public function getStocks()
+    {
+        return $this->hasOne(Stocks::className(), ['id' => 'stocks_id']);
+    }
+}
