c64314fbc500c8fbfc924c09ea081b1da0065e2b
alpo008 <pozhydaev_alexey@mail.ru>
2017-01-13 16:43:55 +0300

    generated mvc for model User

--- models/User.php
+++ models/User.php
@@ -2,7 +2,8 @@
 
 namespace app\models;
 
-use Yii;
+use yii;
+use yii\db\ActiveRecord;
 
 /**
  * This is the model class for table "user".
@@ -15,7 +16,7 @@ use Yii;
  * @property string $role
  * @property string $created
  */
-class User extends \yii\db\ActiveRecord
+class User extends ActiveRecord
 {
     /**
      * @inheritdoc
@@ -58,4 +59,113 @@ class User extends \yii\db\ActiveRecord
             'created' => Yii::t('app', 'Created'),
         ];
     }
+
+    /**
+     * Before save event handler
+     * @param bool $insert
+     * @return bool
+     */
+    public function beforeSave($insert)
+    {
+        if(parent::beforeSave($insert))
+        {
+            if(!empty($this->password))
+            {
+                $this->password = $this->passWithSalt($this->password, 'tuz7773tuz');
+                $this->created = date('Y-m-d');
+            }
+            else
+            {
+                unset($this->password);
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function findIdentity($id)
+    {
+        return static::findOne(['id' => $id]);
+    }
+
+    /**
+     * Finds user by email
+     *
+     * @param  string $email
+     * @return static|null
+     */
+    public static function findByEmail($email)
+    {
+        return static::findOne(['email' => $email]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Validates password
+     *
+     * @param  string $password password to validate
+     * @return boolean if password provided is valid for current user
+     */
+    public function validatePassword ($password)
+    {
+
+        return $this->password === $this->passWithSalt($password, 'Dald7773fEo');
+    }
+
+    /**
+     * @return bool
+     * @internal param IdentityInterface $identity
+     * @internal param int $duration
+     */
+    public function login()
+    {
+        if ($this->validate()) {
+            return Yii::$app->user->login($this->getUser());
+        }
+        return false;
+    }
+
+    public function getUser()
+    {
+        if ($this->_user === false) {
+            $this->_user = $this->findByEmail($this->email);
+        }
+
+        return $this->_user;
+    }
+
+    /**
+     * Returns pass with salt
+     * @param $password
+     * @param $salt
+     * @return string
+     */
+    public function passWithSalt($password, $salt)
+    {
+        return hash('sha512', $password . $salt);
+    }
+
+    /**
+     * Calls ActiveQuery child
+     * @return UsersQuery
+     */
+    public static function find()
+    {
+        return new UsersQuery(get_called_class());
+    }
+
+
 }
